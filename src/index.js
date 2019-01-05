8a378118c81023c0857b35788ee8e9aacf11f1d2
Dave Bot (from Travis CI) <arizonatribe@gmail>
2019-01-05 20:50:25 +0000

    Deploy react-auth-wrapper to github.com/arizonatribe/react-auth-wrapper.git:gh-pages

--- src/index.js
+++
@@ -1,4 +0,0 @@
-export { default as withAuth } from './withAuth'
-export { default as withAuthAndRedirect } from './withAuthAndRedirect'
-export { default as withAuthFromContext } from './withAuthFromContext'
-export { default as withAuthFromContextAndRedirect } from './withAuthFromContextAndRedirect'
